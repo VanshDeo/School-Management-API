afa0501860e8fa286fbf141fbc38ed3c18fe5f26
Vansh Deo <vanshdeo5.mathsg@gmail.com>
2025-04-09 18:32:33 +0530

    Fixed index.js file

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ app.use(express.json());
 app.use('/api', schoolRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0' , () => {
   console.log(`Server is running on port ${PORT}`);
 });
